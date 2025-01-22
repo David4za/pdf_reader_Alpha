52bc3e5080a5d333d9136020ed88d710dd054290
David4za <43610579+David4za@users.noreply.github.com>
2025-01-22 15:16:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,18 @@ def inkoop_text():
     dfs[0].loc[0, "Col_1"] = re.sub(r'([a-z])([A-Z])',r'\1 \2', dfs[0].loc[0, "Col_1"])
     
     # Motor
+    
+    # This part is spliting the motor text and looking if the motor FW is there and then adds the space between e.g., dProIO 
+    original_text = dfs[0].loc[0, "Col_1"]
+    og_split = original_text.split()
+    fw_part = og_split[-1]
+    
+    if fw_part[:-2] in FW_options:
+        updated_fw = fw_part[:-2] + " " + fw_part[-2:]
+        original_text = original_text.replace(fw_part, updated_fw)
+    else:
+        original_text = original_text
+    
     for df in dfs:
         for i in df.index:
             if df.loc[i, "Col_3"] == "nominalmotorvoltage":
